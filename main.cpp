e7730037731918807cdfe2d1c0c48d00f14301e9
Christine Susic <csusic022@insite.4cd.edu>
2026-04-23 20:46:14 -0700

    Commit

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <iomanip>
+#include <string>
 #include <deque>
 #include "Car.h"
 using namespace std;
@@ -9,18 +10,30 @@ using namespace std;
 const int INITIAL_SIZE = 2;
 
 int main() {
-    //deeclarations
+    //declarations
     srand(time(0));
+    //deque of cars
     deque<Car> Cars(INITIAL_SIZE);
+    
+    //two car objects
+    Car car1;
+    Car car2;
+    //push back car objects
+    Cars.push_back(car1);
+    Cars.push_back(car2);
+    //output car objects
+    car1.print();
+    car2.print();
+    car3.print();
     
     //55% probability the car at the head of the line pays and leaves
     Cars.pop_front();
 
     //45% probability that another car joins the line
-    if 
-    Cars.push_back(Car);
+    //if 
+    //Cars.push_back();
 
-    Cars.print();
+  
 
     return 0;
 }
