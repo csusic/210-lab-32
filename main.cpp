6835bbadc01e0957db769b26952af2633314966a
Christine Susic <csusic022@insite.4cd.edu>
2026-04-23 20:56:54 -0700

    Commit

--- main.cpp
+++ main.cpp
@@ -7,7 +7,10 @@
 #include "Car.h"
 using namespace std;
 
+//initial size for the deque of cars
 const int INITIAL_SIZE = 2;
+//minimum and maximum values for random number
+const int MIN = 1, MAX = 100;
 
 int main() {
     //declarations
@@ -15,6 +18,7 @@ int main() {
     //deque of cars
     deque<Car> Cars(INITIAL_SIZE);
     
+    cout << "Initial queue: " << endl;
     //two car objects
     Car car1;
     Car car2;
@@ -25,14 +29,20 @@ int main() {
     car1.print();
     car2.print();
     
-    //55% probability the car at the head of the line pays and leaves
-    Cars.pop_front();
-
-    //45% probability that another car joins the line
-    //if 
-    //Cars.push_back();
-
-  
+    for (int i = 0; i < 6; i++) {
+        //random number
+        int random = rand() % (MAX-MIN+1) + MIN;
+        //55% probability the car at the head of the line pays and leaves
+        if (random <= 55) {
+            Cars.pop_front();
+        }
+        //45% probability that another car joins the line
+        if (random >= 55) {
+            Car car3;
+            Cars.push_back(car3);
+            car3.print();
+        }
+    }
 
     return 0;
 }
